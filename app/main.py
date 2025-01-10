b779b7dc0e262434f27da358cf5689415c1b14a7
Ashutosh Joshi <33080739+ashutoshjoshi1@users.noreply.github.com>
2025-01-10 09:38:25 -0500

    second

--- app/main.py
+++ app/main.py
@@ -7,8 +7,8 @@ def main():
     sys.stdout.write("$ ")
 
     # Wait for user input
-    input()
-
+    command = input()
+    print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
