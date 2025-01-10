fd5a1e98395ae47f508dc18d87610e8c18992afb
Ashutosh Joshi <33080739+ashutoshjoshi1@users.noreply.github.com>
2025-01-10 10:20:00 -0500

    type

--- app/main.py
+++ app/main.py
@@ -1,25 +1,36 @@
 import sys
+import os
 
 
 def main():
     # Uncomment this block to pass the first stage
     # sys.stdout.write("$ ")
     sys.stdout.write("$ ")
-
-    
+    builtin_cmds = ["echo", "exit", "type"]
+    PATH = os.environ.get("PATH")
     # Wait for user input
     while True:
         command = input()
         # if ans := input().strip():
         if command == "exit 0":
             sys.exit(0)
-        elif "type" in command:
-            if command[5:] in ['echo', 'type', 'exit']:
-                print(f"{command[5:]} is a shell builtin")
+        elif command.startswith("type"):
+            cmd = command.split(" ")[1]
+            cmd_path = None
+            paths = PATH.split(":")
+            for path in paths:
+                if os.path.isfile(f"{path}/{cmd}"):
+                    cmd_path = f"{path}/{cmd}"
+            if cmd in builtin_cmds:
+                sys.stdout.write(f"{cmd} is a shell builtin\n")
+                sys.stdout.write("$ ")
+            elif cmd_path:
+                sys.stdout.write(f"{cmd} is {cmd_path}\n")
                 sys.stdout.write("$ ")
             else:
-                sys.stdout.write(f"{command[5:]}: not found\n")
+                sys.stdout.write(f"{cmd} not found\n")
                 sys.stdout.write("$ ")
+            sys.stdout.flush()
         elif "echo" in command:
             print(f"{command[5:]}")
             sys.stdout.write("$ ")
