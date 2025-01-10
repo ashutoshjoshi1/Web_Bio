213132bdfda35789285936e06e61b5c1400c673e
Ashutosh Joshi <33080739+ashutoshjoshi1@users.noreply.github.com>
2025-01-10 10:11:03 -0500

    type

--- app/main.py
+++ app/main.py
@@ -14,10 +14,11 @@ def main():
         if command == "exit 0":
             sys.exit(0)
         elif "type" in command:
-            if "nonexistent" in command:
-                print(f"{command[5:]} not found")
-            else:
+            if command[5:] in ['echo', 'type', 'exit']:
                 print(f"{command[5:]} is a shell builtin")
+                # print(f"{command[5:]} not found")
+            else:
+                sys.stdout.write(f"{command}: command not found\n")
                 sys.stdout.write("$ ")
         elif "echo" in command:
             print(f"{command[5:]}")
